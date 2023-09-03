ab39d84fcea5e1a57cbfaf55c2e69744bbda99c1
xiaomai2000@hotmail.com <xiaomai2000@hotmail.com>
2023-09-03 18:15:05 +0800

    token learn

--- easyocr/ocr-01.py
+++ easyocr/ocr-01.py
@@ -13,7 +13,7 @@ import easyocr
 if __name__ == "__main__":
     print("starts")
     # Changing the image path
-    IMAGE_PATH = r'C:\Users\x\Documents\th.test.02.png'
+    IMAGE_PATH = r'C:\Users\xiaom\Documents\th.test.02.png'
     reader = easyocr.Reader(['id','en'], gpu=False)  #ch_sim, ch_tra, en, th, ko,ja,id,ms  (['th','en']) ['ko','en']  ['ja','en'] ['id','en'] ['ms','en']
     result = reader.readtext(IMAGE_PATH, detail=0)
     print(result)
